325bfa5db9ebe9a454993cf1dbf8e7c3cb81f884
Scott Wang <scotteau@gmail.com>
2020-04-21 05:09:24 +0800

    finished login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,56 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface myProps {}
 
 const LoginForm = (props: myProps) => {
+  //region Style
+  // const debug = `border border-red-500`;
+  const input = `shadow border rounded w-full py-3 px-3 focus:outline-none focus:shadow-outline leading-tight`;
+  const center = `container mx-auto`;
+  const label = `mb-2 block`;
+  const field = `mb-5`;
+  const button = `font-bold py-3 px-5 bg-blue-500 text-white rounded hover:bg-blue-700`;
+  const container = `${center} max-w-md p-8 flex-auto item-center shadow mt-8`;
+  //endregion
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [remembered, setRemembered] = useState("");
+
+  const _state = { username, password, remembered };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(_state);
+  };
+
   return (
-    <div className="w-full max-w-xs">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="username"
-          >
-            Username
-          </label>
+    <div className={container}>
+      <form className={`p-6`} onSubmit={handleSubmit}>
+        <div className={field}>
+          <label className={`${label}`}>Username:</label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
-            type="text"
-            placeholder="Username"
+            type={"text"}
+            name={"username"}
+            placeholder={"Username"}
+            className={input}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
-        <div className="mb-6">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password
-          </label>
+        <div className={`mb-5`}>
+          <label className={label}>Password:</label>
           <input
-            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="Password"
+            type={"password"}
+            name={"password"}
+            placeholder={"Password"}
+            className={input}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <p className="text-red-500 text-xs italic">
-            Please choose a password.
-          </p>
         </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-          >
-            Sign In
-          </button>
-          <a
-            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            href="#"
-          >
-            Forgot Password?
-          </a>
+        <div className={field}>
+          <label>
+            <input
+              type={"checkbox"}
+              className={"mr-2 leading-tight"}
+              value={remembered}
+              onClick={() => setRemembered("yes")}
+            />
+            Remember me
+          </label>
         </div>
+        <button type={"submit"} className={button}>
+          Login
+        </button>
       </form>
     </div>
   );
